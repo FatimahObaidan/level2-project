668d44da96e71bb01c7c0d97a11ce3fac27f28c4
BHanbashi <bhanbashi@gmail.com>
2020-08-25 09:49:05 +0300

    trying to fix index page

--- project-assessment-site/public/js/department.js
+++ project-assessment-site/public/js/department.js
@@ -21,7 +21,7 @@ function displayDepartments(departments) {
     }
 }
 
-const deptUrl = "http://" + window.location.hostname + "/department"
+const deptUrl = "http://" + window.location.hostname + ":31123/department"
 fetch(deptUrl)
     .then(response => response.json())
     .then(data => displayDepartments(data));
